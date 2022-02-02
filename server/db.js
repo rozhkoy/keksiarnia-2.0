391176f481b5bcd00911fcf216dfb2fe6f2fd6e7
zolir <45626144+zolir18@users.noreply.github.com>
2022-02-02 19:07:22 +0100

    add new files

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const {Sequelize} = require('sequelize')
 
+
 module.exports = new Sequelize(
-  process.env.DB_NAME, // Название БД
-  process.env.DB_USER, // Пользователь
-  process.env.DB_PASSWORD, // ПАРОЛЬ
+  "shop", // Название БД
+  "postgres", // Пользователь
+  "ZAQ!2wsx", // ПАРОЛЬ
   {
     host:  'localhost',
     dialect: 'postgres',
-  },
-)
+  })
