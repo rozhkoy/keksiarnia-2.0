caeb6cc5c7cdffd312d2103c3908980e796f60f5
zolir <45626144+zolir18@users.noreply.github.com>
2022-02-02 18:58:46 +0100

    add new files

--- server/index.js
+++ server/index.js
@@ -2,18 +2,20 @@ require("dotenv").config()
 const express = require("express")
 const sequelize = require("./db")
 const model = require("./models/models")
-const PORT = process.env.PORT || 7000
+const PORT = process.env.PORT || 5000
+
 const app = express()
-
-
-
 
 const start = async () => {
   try{
     await sequelize.authenticate()
-    await sequelize.sync()
-    app.listen(PORT,  () => console.log(`start ${PORT}`))
-  }catch (e){
+      .then(() => console.log("yes"))
+    await sequelize.sync().then(
+      () => console.log("create")
+    )
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+  } catch (e) {
     console.log(e)
   }
 }
