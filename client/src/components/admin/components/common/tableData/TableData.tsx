9640a5cf6e4b8bea39c5cf43b97ed3dfd1a4f7c2
zolir <45626144+zolir18@users.noreply.github.com>
2022-02-21 21:47:57 +0100

    add style for table

--- client/src/components/admin/components/common/tableData/TableData.tsx
+++ client/src/components/admin/components/common/tableData/TableData.tsx
@@ -7,29 +7,53 @@ const TableData = () => {
 
 	const table = css`
       width: 100%;
-	  border: 1px solid #000
+	  border: 1px solid #000;
+      caption-side: bottom;
+      border-collapse: collapse;
+
+	`
+	const border = css`
+	border: 1px solid red
+	`
+	const td = css`
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      max-width: 150px;
+	`
+	const edit = css`
+		position: sticky;
+	  right: 0;
 	`
 
+	const tableWrap = css`
+	overflow: scroll`
+
+
+
 	return (
-		<div>
+		<div css={tableWrap}>
 			{
 				<table css={table}>
 					<tr>
 						{
 							mainTypeData.mainType.length > 0 &&
 							Object.keys(mainTypeData.mainType[0]).map((item: string) =>(
-								<th key={item}>{item}</th>
+								<th css={border} key={item}>{item}</th>
 							))
 						}
+						<th css={border}>Edit</th>
 					</tr>
 					{
 						mainTypeData.mainType.map((item: typeValue) => (
 							<tr>
 								{
 									Object.keys(item).map((elem) =>(
-										<td key={item[elem]}>{item[elem]}</td>
+										<td css={[border, td]} key={item[elem]}>{item[elem]}</td>
 									))
+
 								}
+								<td css={[border, td]} ><button>Edit</button></td>
 							</tr>
 						))
 					}
