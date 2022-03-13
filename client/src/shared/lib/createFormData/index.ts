8f86313853d6bdb48c981d5902b61f0d4929f948
zolir <45626144+zolir18@users.noreply.github.com>
2022-03-13 16:43:25 +0100

    add new category

--- client/src/shared/lib/createFormData/index.ts
+++ client/src/shared/lib/createFormData/index.ts
@@ -4,8 +4,6 @@ export function createFormData(obj: Array<ICreateFormData>) {
 	const formData = new FormData();
 	obj.map((item) => {
 		formData.append(item.key, item.value);
-		console.log('asdfsdf', formData.get(item.key));
 	});
-	console.log(formData);
 	return formData;
 }
