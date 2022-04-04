ca0c1442217dd6519250af70bc28ddc91608b3c0
rc.drift18@gmail.com <you@example.com>
2022-04-04 09:46:05 +0200

    added delete and rerendred logic

--- client/src/shared/ui/AdminMultiSelect/types.ts
+++ client/src/shared/ui/AdminMultiSelect/types.ts
@@ -1,6 +1,10 @@
+import { IListItem } from '../AdminCardCreateList/type';
+import { Dispatch, SetStateAction } from 'react';
+
 export type AdminMultiSelectType = {
 	field: string;
 	arrayList: Array<IFilterListForMultiSelect>;
+	getValue: Dispatch<SetStateAction<Array<IFilterListForMultiSelect>>>;
 };
 
 export interface IFilterListForMultiSelect {
